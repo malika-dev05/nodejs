c400e39d2f6ec3af350dfd6329821c364d252797
malika-dev05 <malikasingh0491@gmail.com>
2025-05-20 01:12:28 -0700

    hsdhbg

--- module/fs/file.js
+++ module/fs/file.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 //fs.writeFileSync("./try.txt", "hello mca node js");
 
-let data = fs.readFileSync("./try.txt", "utf-8");
-console.log(data);
+// let data = fs.readFileSync("./try.txt", "utf-8");
+// console.log(data);
 
-console.log("adsasdadasd");
+fs.unlinkSync("./try.txt");
